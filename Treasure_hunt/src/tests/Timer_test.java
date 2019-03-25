a010a99cbf3cc966980120041b2d31dfad3d53dc
Patrik638 <patrik.karlsson.pk@gmail.com>
2019-03-25 18:42:33 +0100

    Merge branch 'master' of https://github.com/Patrik638/Treasure_hunt
    # Conflicts:
    #	Treasure_hunt/src/tests/Timer_test.java

---
+++ Treasure_hunt/src/tests/Timer_test.java
@@ -0,0 +1,28 @@
+package tests;
+
+import java.text.ParseException;
+import org.junit.Test;
+
+public class Timer_test {
+
+	@Test
+	public void isTimeoutTrue() {
+
+	}
+
+	@Test
+	public void isTimeoutFalse() {
+
+	}
+
+	@Test
+	public void timeInitValue() throws ParseException {
+
+	}
+
+	@Test
+	public void timeMaxValue() {
+
+	}
+
+}
