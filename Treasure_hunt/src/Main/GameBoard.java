1430e2cb27825460e948f7769309c1aa8b206bf2
Nicklas Carlsson <ncasv92@hotmail.com>
2019-03-26 21:57:29 +0100

    added three properties, restructured code somewhat.

--- Treasure_hunt/src/Main/GameBoard.java
+++ Treasure_hunt/src/Main/GameBoard.java
@@ -1,164 +1,167 @@
 package Main;
 
-import javax.swing.JTextArea;
+import javax.swing.*;
 
 public class GameBoard {
-	/*
-	 * 	1=vägg
-		2=spelare
-		3=skatt
-		4=dörr
-		5=laser
-	 * 
-	 */
-	public Player player = new Player();
-	int playerRow;
-	int playerCol;
-	private int[][] gameBoard = {
-			{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-			{1,0,0,0,1,0,0,0,0,0,0,3,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-			{1,2,0,0,1,0,0,0,0,0,1,1,1,0,0,5,0,0,0,1,0,0,0,0,0,0,0,0,0,1},
-			{1,0,0,0,1,3,0,0,0,0,0,0,1,0,0,5,0,0,0,1,0,0,0,0,0,0,0,0,0,1},
-			{1,0,0,0,1,1,1,5,5,5,5,5,1,0,0,5,0,3,0,1,0,0,0,0,0,0,0,0,0,1},
-			{1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,1},
-			{1,0,0,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,1},
-			{1,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-			{1,0,0,1,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,4},
-			{1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1},
-			{1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-			{1,1,1,1,0,0,0,0,0,0,0,0,0,1,5,5,5,5,1,0,0,0,0,0,0,0,0,0,0,1},
-			{1,0,0,5,0,1,0,0,0,0,0,0,0,1,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,1},
-			{1,0,0,5,0,1,0,0,0,0,0,0,0,1,0,1,1,0,1,0,0,0,0,0,0,0,0,0,0,1},
-			{1,0,0,1,1,1,0,1,0,0,0,0,0,1,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,1},
-			{1,0,0,0,0,1,0,1,0,0,0,0,0,1,0,1,1,0,1,0,0,0,0,0,0,0,0,0,0,1},
-			{1,0,0,0,0,1,0,1,0,0,0,0,0,1,0,1,3,0,1,0,0,0,0,0,0,0,0,0,0,1},
-			{1,0,0,0,3,1,0,1,1,0,0,0,0,1,3,1,1,0,1,0,0,0,0,0,0,0,0,0,0,1},
-			{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}};
-	
-	public GameBoard() {
-		
-	}
+    /*
+     * 	1=vägg
+        2=spelare
+        3=skatt
+        4=dörr
+        5=laser
+     *
+     */
+    public Player player = new Player();
+    int playerRow;
+    int playerCol;
+    private int[][] gameBoard = {
+            {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+            {1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 3, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            {1, 2, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 5, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            {1, 0, 0, 0, 1, 3, 0, 0, 0, 0, 0, 0, 1, 0, 0, 5, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            {1, 0, 0, 0, 1, 1, 1, 5, 5, 5, 5, 5, 1, 0, 0, 5, 0, 3, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            {1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            {1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            {1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4},
+            {1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            {1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            {1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 5, 5, 5, 5, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            {1, 0, 0, 5, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            {1, 0, 0, 5, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            {1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            {1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            {1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 3, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            {1, 0, 0, 0, 3, 1, 0, 1, 1, 0, 0, 0, 0, 1, 3, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+            {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}};
 
+    public GameBoard() {
 
-	public void updatePlayerPosition(){
-	    playerRow=player.getPlayerRowNumber();
-	    playerCol=player.getPlayerColNumber();
     }
-	public int[][] getGameBoard() {
-		return gameBoard;
-	}
-	
-	public void setGameBoard(int[][] gameBoard) {
-		this.gameBoard = gameBoard;
-	}
 
-	public int getValueOfPosition(int row, int col) {
-		return gameBoard[row][col];
-	}
 
-	public int getNumberOfRows(){
-		return gameBoard.length;
-	}
-	public int getNumberOfCols(){
-		return gameBoard[0].length;
-	}
+    public void updatePlayerPosition() {
+        playerRow = player.getPlayerRowNumber();
+        playerCol = player.getPlayerColNumber();
+    }
 
-	public void setValueOfPosition(int row, int col, int intToSet){
-		gameBoard[row][col]=intToSet;
-	}
+    public int[][] getGameBoard() {
+        return gameBoard;
+    }
 
-	public JTextArea print(JTextArea text) {
-		for (int row = 0; row < 19; row++) {
-			for (int col = 0; col < 30; col++) {
-				text.append(Integer.toString(gameBoard[row][col]));
-			}
-			text.append("\n");
-		}
-		return text;
-	}
+    public void setGameBoard(int[][] gameBoard) {
+        this.gameBoard = gameBoard;
+    }
 
-	public int getIntLeftFromPlayer(){
-	    updatePlayerPosition();
-		return getValueOfPosition(playerRow,playerCol-1);
-	}
+    public int getValueOfPosition(int row, int col) {
+        return gameBoard[row][col];
+    }
 
-	public int getIntRightFromPlayer(){
+    public void setValueOfPosition(int row, int col, int intToSet) {
+        gameBoard[row][col] = intToSet;
+    }
+
+    public int getNumberOfRows() {
+        return gameBoard.length;
+    }
+
+    public int getNumberOfCols() {
+        return gameBoard[0].length;
+    }
+
+
+    public JTextArea print(JTextArea text) {
+        for (int row = 0; row < 19; row++) {
+            for (int col = 0; col < 30; col++) {
+                text.append(Integer.toString(gameBoard[row][col]));
+            }
+            text.append("\n");
+        }
+        return text;
+    }
+
+    public int getIntLeftFromPlayer() {
         updatePlayerPosition();
-		return getValueOfPosition(playerRow,playerCol+1);
-	}
+        return getValueOfPosition(playerRow, playerCol - 1);
+    }
 
-	public int getIntUpFromPlayer(){
+    public int getIntRightFromPlayer() {
         updatePlayerPosition();
-		return getValueOfPosition(playerRow-1,playerCol);
-	}
-	public int getIntDownFromPlayer(){
-	    updatePlayerPosition();
-		return getValueOfPosition(playerRow+1,playerCol);
-	}
+        return getValueOfPosition(playerRow, playerCol + 1);
+    }
 
-	public boolean playerWantToMoveLeft() {
-		if (player.movePlayerLeft(getIntLeftFromPlayer())){
-			updatePlayerPosition();
-            setValueOfPosition(playerRow,playerCol,2);
+    public int getIntUpFromPlayer() {
+        updatePlayerPosition();
+        return getValueOfPosition(playerRow - 1, playerCol);
+    }
+
+    public int getIntDownFromPlayer() {
+        updatePlayerPosition();
+        return getValueOfPosition(playerRow + 1, playerCol);
+    }
+
+    public boolean playerWantToMoveLeft() {
+        if (player.movePlayerLeft(getIntLeftFromPlayer())) {
+            updatePlayerPosition();
+            setValueOfPosition(playerRow, playerCol, 2);
             //player moved. Setting previous location to 0
-            setValueOfPosition(playerRow,playerCol+1,0);
+            setValueOfPosition(playerRow, playerCol + 1, 0);
             return true;
         }
-		return false;
-	}
+        return false;
+    }
 
-	public boolean playerWantToMoveRight() {
-		if (player.movePlayerRight(getIntRightFromPlayer())){
-			updatePlayerPosition();
-			setValueOfPosition(playerRow,playerCol,2);
-			//player moved. Setting previous location to 0
-			setValueOfPosition(playerRow,playerCol-1,0);
-			return true;
-		}
-		return false;
-	}
+    public boolean playerWantToMoveRight() {
+        if (player.movePlayerRight(getIntRightFromPlayer())) {
+            updatePlayerPosition();
+            setValueOfPosition(playerRow, playerCol, 2);
+            //player moved. Setting previous location to 0
+            setValueOfPosition(playerRow, playerCol - 1, 0);
+            return true;
+        }
+        return false;
+    }
 
     public boolean playerWantToMoveUp() {
-        if (player.movePlayerUp(getIntUpFromPlayer())){
+        if (player.movePlayerUp(getIntUpFromPlayer())) {
             updatePlayerPosition();
-            setValueOfPosition(playerRow,playerCol,2);
+            setValueOfPosition(playerRow, playerCol, 2);
             //player moved. Setting previous location to 0
-            setValueOfPosition(playerRow+1,playerCol,0);
+            setValueOfPosition(playerRow + 1, playerCol, 0);
             return true;
         }
         return false;
     }
 
     public boolean playerWantToMoveDown() {
-        if (player.movePlayerDown(getIntDownFromPlayer())){
+        if (player.movePlayerDown(getIntDownFromPlayer())) {
             updatePlayerPosition();
-            setValueOfPosition(playerRow,playerCol,2);
+            setValueOfPosition(playerRow, playerCol, 2);
             //player moved. Setting previous location to 0
-            setValueOfPosition(playerRow-1,playerCol,0);
+            setValueOfPosition(playerRow - 1, playerCol, 0);
             return true;
         }
         return false;
     }
 
 
+    public void setPlayerPosition(int row, int col) {
+        // Setting old position to 0
+        setValueOfPosition(player.getPlayerRowNumber(), player.getPlayerColNumber(), 0);
+        // Changing location to player
 
-	public void setPlayerPosition(int row,int col){
-		// Setting old position to 0
-		setValueOfPosition(player.getPlayerRowNumber(),player.getPlayerColNumber(),0);
-		// Changing location to player
-
-		player.setPlayerPosition(row,col);
-		updatePlayerPosition();
-		// setting position of player to board
-		setValueOfPosition(row,col,2);
-	}
+        player.setPlayerPosition(row, col);
+        updatePlayerPosition();
+        // setting position of player to board
+        setValueOfPosition(row, col, 2);
+    }
 
 
-	public void disableDoor(int row, int col) {
-		setValueOfPosition(row, col, 1);
-	}
-	
-	public void enableDoor(int row, int col) {
-		setValueOfPosition(row, col, 4);
-	}
+    public void disableDoor(int row, int col) {
+        setValueOfPosition(row, col, 1);
+    }
+
+    public void enableDoor(int row, int col) {
+        setValueOfPosition(row, col, 4);
+    }
 }
--- Treasure_hunt/src/Main/GameSession.java
+++ Treasure_hunt/src/Main/GameSession.java
@@ -1,86 +1,92 @@
 package Main;
 
-import java.awt.Font;
-import java.awt.event.*;
-import javax.swing.JFrame;
-import javax.swing.JTextArea;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 @SuppressWarnings("serial")
 public class GameSession extends JFrame implements Runnable, KeyListener {
 
-	private Thread newThread;
-	public GameBoard gameBoard = new GameBoard();
-	public boolean gameOver = false;
-	public JFrame frame = new JFrame("Treasure hunt");
-	public JTextArea text;
+    public GameBoard gameBoard = new GameBoard();
 
-	public GameSession() {
-		frame.setLayout(null);
-		frame.setSize(600,620);
-		frame.setLocation(100,50);
-		frame.addWindowListener(new WindowAdapter() {
-			@Override
-			public void windowClosing(WindowEvent e) {
-				System.exit(0);
-			}
-		});
+    public boolean gameOver = false;
+    public boolean timeOut = false;
+    public boolean youWon = false;
 
-		text = new JTextArea();
-		text.addKeyListener(this);
-		text.setBounds(20, 20, 600, 550);
-		text.setFont(new Font("Arial", Font.PLAIN, 24));
-		frame.add(text);
-		frame.setVisible(true);
+    public int numberOfFoundTreasures = 0;
+    public JFrame frame = new JFrame("Treasure hunt");
+    public JTextArea text;
+    private Thread newThread;
 
-		//Skapar en ny tråd
-		newThread = new Thread(this);
-		newThread.start();
-	}
+    public GameSession() {
+        frame.setLayout(null);
+        frame.setSize(600, 620);
+        frame.setLocation(100, 50);
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+
+        text = new JTextArea();
+        text.addKeyListener(this);
+        text.setBounds(20, 20, 600, 550);
+        text.setFont(new Font("Arial", Font.PLAIN, 24));
+        frame.add(text);
+        frame.setVisible(true);
+
+        //Skapar en ny tråd
+        newThread = new Thread(this);
+        newThread.start();
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        new GameSession();
+    }
 
     public void keyTyped(KeyEvent e) {
         char detectedChar = e.getKeyChar();
         detectedChar = Character.toLowerCase(detectedChar);
         switch (detectedChar) {
             case 'w':
-				gameBoard.playerWantToMoveUp();
+                gameBoard.playerWantToMoveUp();
                 break;
             case 'a':
                 gameBoard.playerWantToMoveLeft();
                 break;
             case 's':
-				gameBoard.playerWantToMoveDown();
-				break;
+                gameBoard.playerWantToMoveDown();
+                break;
             case 'd':
-				gameBoard.playerWantToMoveRight();
+                gameBoard.playerWantToMoveRight();
                 break;
             default:
                 // Do nothing
         }
     }
 
-	public void keyReleased(KeyEvent e) {
+    public void keyReleased(KeyEvent e) {
 
-	}
+    }
 
     public void keyPressed(KeyEvent e) {
 
-	}
+    }
 
-	public void run() {
-		gameBoard.disableDoor(8, 29);
-		while(!gameOver)
-		{
-			text.setText("");
-			text = gameBoard.print(text);
-			try {
-				Thread.sleep(300);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-
-	public static void main(String[] args) throws InterruptedException {
-		new GameSession();
-	}
+    public void run() {
+        gameBoard.disableDoor(8, 29);
+        while (!gameOver) {
+            text.setText("");
+            text = gameBoard.print(text);
+            try {
+                Thread.sleep(300);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
